6fabf9e06ff14309dfde5b215dabf7f7bd2374ae
3minhaz <3minhaz@gmail.com>
2021-10-30 21:10:48 +0600

    id updated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const email = require('mongodb').email;
 require('dotenv').config();
 
 
